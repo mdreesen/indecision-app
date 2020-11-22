1a81c2c7e3b7130d473b55b6d13df167110a55ba
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-21 20:54:57 -0700

    added arrow functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,45 @@
-"use strict";
+'use strict';
+
+/*
+const otherFunction = function() {
+    return x * x
+};
+
+function square(x) {
+   return x * x
+};
+
+// using these functions, don't need returns
+const squareArrow = (x) => {
+    return x * x;
+};
+*/
+
+// using these functions, don't need returns
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(9));
+// console.log(squareArrow(4));
+
+// challenge, arrow functions
+// get firstName
+// create regular arrow function
+// create arrow functions using the short syntax
+
+//const firstName = 'Michael Dreesen';
+
+// const fullName = 'Michael Dreesen';
+//let firstName;
+
+// get firstName function
+/*
+const getFirstName = (fullName) => {
+    return fullName.split(' ')[0]
+};
+*/
+// get firstName function
+var getFirstName = function getFirstName(fullName) {
+    return fullName.split(' ')[0];
+};
+
+console.log(getFirstName('Michael Dreesen'));
